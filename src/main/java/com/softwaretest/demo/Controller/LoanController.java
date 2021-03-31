34bfe4f837bf1c8a51449307d037cbe01af98c7d
guo taian <18302010041@fudan.edu.cn>
2021-03-31 20:03:27 +0800

    初步搭建框架

---
+++ src/main/java/com/softwaretest/demo/Controller/LoanController.java
@@ -0,0 +1,17 @@
+package com.softwaretest.demo.Controller;
+
+import com.softwaretest.demo.Service.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoanController {
+    @Autowired
+    private LoanService loanService;
+
+
+
+
+
+}
+
---
+++ src/main/java/com/softwaretest/demo/Service/LoanService.java
@@ -0,0 +1,9 @@
+package com.softwaretest.demo.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanService {
+
+
+}
