0577404cd3297ce6a475c705f14c6731ea518875
guo taian <18302010041@fudan.edu.cn>
2021-04-07 18:49:48 +0800

    初步设计了数据库表

--- src/main/java/com/softwaretest/demo/Service/LoanService.java
+++ src/main/java/com/softwaretest/demo/Service/LoanService.java
@@ -1,9 +1,32 @@
 package com.softwaretest.demo.Service;
 
+import com.softwaretest.demo.Controller.Response.CheckResponse;
+import com.softwaretest.demo.Domain.Account;
+import com.softwaretest.demo.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class LoanService {
+    @Autowired
+    private AccountRepository accountRepository;
 
+    @Autowired
+    public LoanService(){
 
+    }
+
+    public List<CheckResponse> getLoans(String idNumber){
+        List<Account> accounts = accountRepository.findByIdNumber(idNumber);
+        if(accounts == null){
+            return null;
+        }
+
+        List<CheckResponse> checkResponses = new LinkedList<>();
+
+        return checkResponses;
+    }
 }
